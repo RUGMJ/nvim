7e23509188cbea2dab9471e0d8dba27c64322e08
rugmj <rugmj@rugmj.dev>
2024-12-07 00:27:30 +0000

    feat: Adds treesitter folding

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.o.relativenumber = true
 vim.o.cmdheight = 0
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.foldlevelstart = 99
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,9 @@ return {
   },
   build = ':TSUpdate',
   config = function()
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
     local configs = require("nvim-treesitter.configs")
     configs.setup({
       ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },

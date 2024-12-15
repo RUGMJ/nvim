694e81787f866aacd708658041684a515e87389b
rugmj <rugmj@rugmj.dev>
2024-12-15 17:16:05 +0000

    feat: changes how finding files works

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,13 +40,14 @@ return {
     end, { desc = '[/] Fuzzily search in current buffer' })
 
     vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>ff', function()
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fF', function()
       require('telescope.builtin').find_files {
         hidden = true,
         no_ignore = true,
         no_ignore_parent = true
       }
-    end, { desc = '[F]ind [F]iles' })
+    end, { desc = '[F]ind All [F]iles' })
     vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
     vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', require('telescope').extensions.live_grep_args.live_grep_args,

b03c4b5be06250c721d1b0940f6e10ee1d2b068f
rugmj <rugmj@rugmj.dev>
2025-01-21 19:55:14 +0000

    feat(looks): makes some ui changes makes the background transparent

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,14 @@
 return {
   'catppuccin/nvim',
   name = "catppuccin",
+  priority = 1000,
   config = function()
     require("catppuccin").setup {
+      transparent_background = true,
+
       integrations = {
+        blink_cmp = true,
+        fidget = true
       }
     }
     vim.cmd.colorscheme("catppuccin")
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,7 +4,16 @@ return {
     { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
 
-    { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+    {
+      'j-hui/fidget.nvim',
+      opts = {
+        notification = {
+          window = {
+            winblend = 0, -- makes catppuccin happy
+          },
+        }
+      }
+    },
 
     {
       "folke/lazydev.nvim",

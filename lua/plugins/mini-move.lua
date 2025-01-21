748351d5eccb7088a36e16078baba3c8432a9760
rugmj <rugmj@rugmj.dev>
2025-01-21 17:37:06 +0000

    feat(mini-move): Adds mini-move

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,21 @@
+return {
+  'echasnovski/mini.move',
+  version = false,
+  opts = {
+    mappings = {
+      left = '<S-Left>',
+      right = '<S-Right>',
+      down = '<S-Down>',
+      up = '<S-Up>',
+
+      line_left = '<S-Left>',
+      line_right = '<S-Right>',
+      line_down = '<S-Down>',
+      line_up = '<S-Up>',
+    },
+
+    options = {
+      reindent_linewise = true,
+    },
+  }
+}

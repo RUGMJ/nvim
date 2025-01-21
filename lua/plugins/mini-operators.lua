7f08cdff85b94896dd5badd43811827b66beb7ef
rugmj <rugmj@rugmj.dev>
2025-01-21 17:36:53 +0000

    feat(mini-operators): adds mini-operators

---
+++ lua/plugins/mini-operators.lua
@@ -0,0 +1,35 @@
+return {
+  'echasnovski/mini.operators',
+  version = false,
+  opts = {
+    evaluate = {
+      func = nil,
+
+      prefix = 'g=',
+    },
+
+    exchange = {
+      prefix = 'ge',
+
+      reindent_linewise = true,
+    },
+
+    multiply = {
+      prefix = 'gm',
+
+      func = nil,
+    },
+
+    replace = {
+      prefix = 'gr',
+
+      reindent_linewise = true,
+    },
+
+    sort = {
+      prefix = 'gs',
+
+      func = nil,
+    }
+  }
+}

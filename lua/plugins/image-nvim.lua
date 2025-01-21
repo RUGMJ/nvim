0a544df95540200d3c7ebc1c6641e6d985e982d7
rugmj <rugmj@rugmj.dev>
2025-01-21 17:35:48 +0000

    chore(image-nvim): disable

--- lua/plugins/image-nvim.lua
+++ lua/plugins/image-nvim.lua
@@ -1,6 +1,7 @@
 return {
   "3rd/image.nvim",
-  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  build = false,   -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  enabled = false, -- still trying to get this to work with my workflow
   opts = {
     processor = "magick_cli",
     backend = "kitty",

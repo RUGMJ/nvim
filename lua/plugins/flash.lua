5213beafeddf4a9c453485e3a57bfac34f130d6e
rugmj <rugmj@rugmj.dev>
2025-03-02 11:29:16 +0000

    feat(flash): Disables the `f` and `t` overrides

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,13 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        enabled = false
+      }
+    }
+  },
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
